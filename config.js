b396bd66349581312392eac82278f3c922efcbc6
slhachbro <149378570+slhachbro@users.noreply.github.com>
2023-10-31 09:41:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94764338173', 'me', true],
+  ['94764338173', 'sl ip', true], 
   [''] 
 ] //Number of owners
 
